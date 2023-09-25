c89873dc221bc3187ffd66a1b4b27d4a279e5a74
csooriyakumaran <c.sooriyakumaran@gmail.com>
2023-09-24 23:56:28 -0400

    init

--- build/Debug-windows-x86_64/ludwig/main.d
+++
@@ -1,6 +0,0 @@
-build/Debug-windows-x86_64/ludwig/main.o: src/main.cpp \
- src/ludwig/core/types.h src/ludwig/core/linspace.h \
- src/ludwig/mesh/uniform-grid.h
-src/ludwig/core/types.h:
-src/ludwig/core/linspace.h:
-src/ludwig/mesh/uniform-grid.h:
